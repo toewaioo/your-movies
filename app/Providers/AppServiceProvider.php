8f989ce74c2e8404a46ebcd8062d38ea78b51504
Toewaioo <sawtoewaioo2003@gmail.com>
2025-11-06 13:31:38 +0630

    init#7

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,8 @@ namespace App\Providers;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
+use Illuminate\Support\Facades\URL;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -21,5 +23,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Vite::prefetch(concurrency: 3);
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
