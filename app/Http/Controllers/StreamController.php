b7d516a448a1595444120934f8d67b1221d0feb5
Toewaioo <sawtoewaioo2003@gmail.com>
2025-11-17 16:13:45 +0630

    add streaming#1

--- app/Http/Controllers/StreamController.php
+++ app/Http/Controllers/StreamController.php
@@ -25,20 +25,38 @@ class StreamController extends Controller
 
         $opts = [
             "http" => [
-                "method" => "GET",
-                "header" => implode("\r\n", $headers)
+                "method"  => "GET",
+                "header"  => implode("\r\n", $headers)
             ]
         ];
 
         $context = stream_context_create($opts);
 
-        $stream = fopen($url, "rb", false, $context);
+        $stream = @fopen($url, "rb", false, $context);
 
-        foreach ($http_response_header as $header) {
-            if (stripos($header, 'Content-Type:') === 0) header($header);
-            if (stripos($header, 'Content-Length:') === 0) header($header);
-            if (stripos($header, 'Accept-Ranges:') === 0) header($header);
-            if (stripos($header, 'Content-Range:') === 0) header($header);
+        if (!$stream) {
+            abort(404, "Unable to open remote file.");
+        }
+
+        // Default MIME if remote does not send one
+        $mimeType = "video/mp4";
+
+        // Read remote headers
+        foreach ($http_response_header as $h) {
+            if (stripos($h, "Content-Type:") === 0) {
+                header($h);
+                $mimeType = trim(substr($h, 13));
+            }
+
+            if (stripos($h, "Content-Length:") === 0) header($h);
+            if (stripos($h, "Content-Range:") === 0)  header($h);
+            if (stripos($h, "Accept-Ranges:") === 0) header($h);
+        }
+
+        // If server never sent MIME → manually set correct one
+        if (!headers_sent()) {
+            header("Content-Type: $mimeType");
+            header("Accept-Ranges: bytes");
         }
 
         fpassthru($stream);
