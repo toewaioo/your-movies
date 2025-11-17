75857d4913c9dddb3e923656a5c8738b472dbe20
Toewaioo <sawtoewaioo2003@gmail.com>
2025-11-17 17:29:29 +0630

    add streaming#3

--- app/Http/Controllers/DownloadController.php
+++ app/Http/Controllers/DownloadController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Helpers\SecureStream;
 use App\Helpers\MimeDetector;
+use Illuminate\Support\Facades\Http;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 
 class DownloadController extends Controller
 {
@@ -18,31 +20,34 @@ class DownloadController extends Controller
 
     private function forceDownload($url)
     {
-        $stream = @fopen($url, "rb");
-
-        if (!$stream) abort(404, "File not accessible");
-
-        // Auto detect MIME type
-        $mime = MimeDetector::detect($url);
-
-        // Guess filename from URL
-        $name = basename(parse_url($url, PHP_URL_PATH)) ?: "download.bin";
-
-        // Force download headers
-        header("Content-Type: $mime");
-        header("Content-Disposition: attachment; filename=\"$name\"");
-        header("Content-Transfer-Encoding: binary");
-        header("Accept-Ranges: bytes");
-
-        // Optional: If remote headers include content-length
-        global $http_response_header;
-        foreach ($http_response_header as $h) {
-            if (stripos($h, "Content-Length:") === 0) {
-                header($h);
-            }
+        // Try to get file headers
+        try {
+            $head = Http::head($url);
+            $mime = $head->header('Content-Type') ?? 'application/octet-stream';
+            $size = $head->header('Content-Length');
+        } catch (\Exception $e) {
+            $mime = 'application/octet-stream';
+            $size = null;
         }
 
-        fpassthru($stream);
-        exit;
+        // Extract filename
+        $filename = basename(parse_url($url, PHP_URL_PATH));
+
+        // Stream file to browser
+        return new StreamedResponse(function () use ($url) {
+            $stream = fopen($url, 'r');
+            while (!feof($stream)) {
+                echo fread($stream, 1024 * 16); // 16 KB chunks
+                flush();
+            }
+            fclose($stream);
+        }, 200, [
+            "Content-Type" => $mime,
+            "Content-Disposition" => "attachment; filename=\"$filename\"",
+            "Content-Length" => $size,
+            "Cache-Control" => "no-cache, no-store, must-revalidate",
+            "Pragma" => "no-cache",
+            "Expires" => "0",
+        ]);
     }
 }
