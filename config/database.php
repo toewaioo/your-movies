5b72d9ae3b5479d6d16c2091f2482c85c28dc58e
Toewaioo <sawtoewaioo2003@gmail.com>
2025-11-29 17:09:06 +0630

    init#13

--- config/database.php
+++ config/database.php
@@ -94,7 +94,7 @@ return [
             'charset' => env('DB_CHARSET', 'utf8'),
             'prefix' => '',
             'prefix_indexes' => true,
-            'search_path' => 'public',
+            'search_path' => 'movies',
             'sslmode' => 'prefer',
         ],
 
